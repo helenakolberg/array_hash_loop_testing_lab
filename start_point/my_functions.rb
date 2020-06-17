aa52747a72f08e1009c6515e9a0404ec0f5c1bfc
James Monks <J.monks23@gmail.com>
2020-06-17 14:39:43 +0100

    sum array pass

--- start_point/my_functions.rb
+++ start_point/my_functions.rb
@@ -1,3 +1,11 @@
 def add_array_lengths(array_1, array_2)
     return array_1.length + array_2.length
 end
+
+def sum_array(numbers)
+    result = 0
+    for number in numbers
+        result += number
+    end
+    return result
+end
